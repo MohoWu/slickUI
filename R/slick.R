901b2212dc63d1f8a1d74bc93d8ee1d2deadd147
Hao Wu <hao.wu@argusmedia.com>
2022-05-27 16:21:09 +0100

    add options to slick()

--- R/slick.R
+++ R/slick.R
@@ -1,16 +1,39 @@
 #' Slick carousel
 #'
-#' @param ... Div elements to show in the slider.
+#' @param ui Elements to show in the slider.
+#' @param dots Whether to show dots.
+#' @param infinite infinite scrolling
+#' @param slidesToShow number of slides to show in each window
+#' @param slidesToScroll number of slides to scroll each time
 #'
 #' @return
 #' @export
 #'
-#' @examples slick(shiny::div("content1"), shiny::div("content2"))
-slick <- function(...) {
+#' @examples slick(ui = shiny::tagList(shiny::div("content1"), shiny::div("content2")))
+slick <- function(dots = TRUE,
+                  infinite = TRUE,
+                  slidesToShow = 1,
+                  slidesToScroll = 1,
+                  ui) {
 
-  shiny::div(
-    class = "slider",
-    ...
+  shiny::tagList(
+
+		shiny::div(
+		  class = "slider",
+		  ui
+		),
+		tags$script(HTML(
+		  glue::glue(
+		    "$('.slider').slick({
+			dots: {{tolower(dots)}},
+			infinite: {{tolower(infinite)}},
+			slidesToShow: {{slidesToShow}},
+			slidesToScroll: {{slidesToScroll}}
+		});
+		console.log(`slider-configured`);",
+		.open = "{{", .close = "}}")
+		))
   )
+
 
 }
--- inst/slickUI/slickUI.js
+++ inst/slickUI/slickUI.js
@@ -1,5 +1,16 @@
-$(document).ready(function () {
-	$('.slider').slick({
-		dots: true
-	})
-});
+// $(document).ready(function () {
+// 	$('.slider').slick({
+// 		dots: true
+// 	})
+// });
+// $(function () {
+// 	Shiny.addCustomMessageHandler('create-winbox', function (data) {
+// 		$('.slider').slick({
+// 			dots: data.dots,
+// 			infinite: data.infinite,
+// 			slidesToShow: data.slidesToShow,
+// 			slidesToScroll: data.slidesToScroll
+// 		});
+// 		console.log(`slider-configured`);
+// 	});
+// });
